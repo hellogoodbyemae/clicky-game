d76e9cac5899f44bff3f17eaddeb07990d12a204
Margaret Egner <hellogoodbyemae@gmail.com>
2020-08-22 23:15:04 -0400

    deploy success

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,8 +3,8 @@ import "./Jumbotron.css";
 
 const Jumbotron = props => (
     <div className="jumbotron">{props.children}
-        <h1 className="display-4">Clicky Game!</h1>
-        <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
+        <h1 className="display-4">The Office Edition!</h1>
+        <h3 className="lead">Click on an image to earn points, but don't click on any more than once!</h3>
     </div>
 )
 export default Jumbotron;
