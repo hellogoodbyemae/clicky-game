ae23efbe3f9f783a69be7001f1d27d9be164dbc8
Margaret Egner <hellogoodbyemae@gmail.com>
2020-08-22 22:51:50 -0400

    Updates

---
+++ precache-manifest.ace185fc79e95fde0620da2f5cc83e44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291e38914fa589f8fbf97a10912d2140",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "976d3072ebe19cf08c15",
+    "url": "/clicky-game/static/css/main.9783f9cc.chunk.css"
+  },
+  {
+    "revision": "1ed7412eeb18d073fdb0",
+    "url": "/clicky-game/static/js/2.23ca1912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.23ca1912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976d3072ebe19cf08c15",
+    "url": "/clicky-game/static/js/main.ae03852f.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  },
+  {
+    "revision": "a1d6fcd4eee695ca240ffd7c4753694a",
+    "url": "/clicky-game/static/media/The_Office-logo.a1d6fcd4.png"
+  },
+  {
+    "revision": "a583478e8c66571d27a0793845d81b08",
+    "url": "/clicky-game/static/media/background.a583478e.jpg"
+  }
+]);
